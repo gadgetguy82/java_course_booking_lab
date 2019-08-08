2e8239504e959a194a49765bfb6828761e938a09
Robert Tam <rsh.tam@gmail.com>
2019-08-08 10:48:07 +0100

    Add Course class and test constructor

--- src/test/java/com/codeclan_lab/coursebookingsystem/CoursebookingsystemApplicationTests.java
+++ src/test/java/com/codeclan_lab/coursebookingsystem/CoursebookingsystemApplicationTests.java
@@ -1,9 +1,12 @@
 package com.codeclan_lab.coursebookingsystem;
 
+import com.codeclan_lab.coursebookingsystem.models.Course;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +14,12 @@ public class CoursebookingsystemApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void canCreateCourse() {
+		Course course = new Course("Ruby", "Edinburgh", 5);
+		assertEquals("Ruby", course.getName());
 	}
 
 }
