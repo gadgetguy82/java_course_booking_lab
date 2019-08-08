a9c46e918aa2e6597f2877c73b232109a83c943a
Robert Tam <rsh.tam@gmail.com>
2019-08-08 10:39:24 +0100

    Add files to repo

---
+++ src/main/java/com/codeclan_lab/coursebookingsystem/CoursebookingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan_lab.coursebookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursebookingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursebookingsystemApplication.class, args);
+	}
+
+}
